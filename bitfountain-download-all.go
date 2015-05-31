df79ac273efda12f2c1f61dc225ff5c68e311af2
Ben <othyn@users.noreply.github.com>
2015-05-31 03:16:02 +0100

    Fixes file name bug and adds various functionality
    fixes prateekbhatt/bitfountain-download-all#3
    
    Adds / Changes:
        Adds a progress bar for the download
        Adds a new optional parameter ’-section’ to allow you to specify a
    starting section for the download
        Formats the raw byte string into KB/MB for better readability
        Fixes issues with special characters within the filenames / folders

--- bitfountain-download-all.go
+++ bitfountain-download-all.go
@@ -18,15 +18,30 @@ import (
 	// "reflect"
 	// "crypto/ssh/terminal"
 	// "github.com/tobyhede/go-underscore"
+
+	"github.com/pivotal-golang/bytefmt"
+	// For Byte formatting on the video size
+
+	"regexp"
+	// For filtering out any special characters in the names
+
+	"time"
+	"github.com/cheggaaa/pb"
+	// For the progress bar
 )
 
 func getDashedName(name string, index int) string {
 
-	trimmedName := strings.TrimSpace(name)
-	splitNameBySpace := strings.Split(trimmedName, " ")
-	dashedName := strings.Join(splitNameBySpace, "-")
-	dashedName = fmt.Sprint(strconv.Itoa(index), "-", dashedName)
-	return dashedName
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	if err != nil {
+		log.Fatal( err )
+	}
+	safe := reg.ReplaceAllString( name, "-" )
+	safe = fmt.Sprint( strconv.Itoa(index), "-", strings.Trim( safe, "-" ) )
+	return safe
+	// Filters out all special characters
+	//	This is to fix the directory error when hitting a lecture name with a forward slash in it
+	//	along with any other potential issues with special characters within the filenames
 }
 
 // func getLatestDownloadedVideo() int {
@@ -41,11 +56,12 @@ func main() {
 	SCHOOL_ID := "24" // id of bitfountain on usefedora.com
 
 	// commandline inputs for email, password, and course url
-	emailPtr := flag.String("email", "", "Email of the user")
+	emailPtr	 := flag.String("email", "", "Email of the user")
 	courseUrlPtr := flag.String("course", "", "URL of the course")
-	passwordPtr := flag.String("pass", "", "Password of the user")
-
-	// optionalStartLessonNoPtr := flag.String("no", "", "Lesson no, starts at 0")
+	passwordPtr	 := flag.String("pass", "", "Password of the user")
+	
+	optionalStartSectionNoPtr := flag.Int( "section", 0, "Section number to start the downloads at, starts at 0" )
+	// Optional start lecture for the download process
 
 	flag.Parse()
 
@@ -60,6 +76,10 @@ func main() {
 	if *courseUrlPtr == "" {
 		log.Fatal("Please provide the bitfountain course url")
 	}
+
+	fmt.Printf( "\nStarting download at section %d ...\n", *optionalStartSectionNoPtr )
+	// Display the starting course for the users reference
+	//	Unnecessary, I just like it like that :)
 
 	// pass, err := terminal.ReadPassword()
 	// if err != nil {
@@ -123,52 +143,62 @@ func main() {
 	// Find all the sections in the course, and loop over them
 	doc.Find(".course-section").Each(func(i int, s *goquery.Selection) {
 
-		// Get the name of the current section
-		name := s.Find(".section-title").Text()
+		if i >= *optionalStartSectionNoPtr {
 
-		name = getDashedName(name, i)
-		sectionDir := filepath.Join(courseDir, name)
+			// Only add the section if it is above the requested section index
 
-		newLectures := []Lecture{}
+			// Get the name of the current section
+			name := s.Find(".section-title").Text()
 
-		// Find all the lectures in the current section, and loop over them
-		s.Find(".section-item").Each(func(i int, l *goquery.Selection) {
+			name = getDashedName(name, i)
+			sectionDir := filepath.Join(courseDir, name)
 
-			// Get the name of the lecture (video)
-			lectureName := l.Find(".lecture-name").Text()
+			newLectures := []Lecture{}
 
-			// Get the lecture id from the attribute. This will be used to
-			// construct the url of each lecture's page
-			lectureId, _ := l.Attr("data-lecture-id")
+			// Find all the lectures in the current section, and loop over them
+			s.Find(".section-item").Each(func(i int, l *goquery.Selection) {
 
-			lectureId = strings.TrimSpace(lectureId)
+				// Get the name of the lecture (video)
+				lectureName := l.Find(".lecture-name").Text()
 
-			lectureFileName := fmt.Sprint(getDashedName(lectureName, i), ".mp4")
+				// Get the lecture id from the attribute. This will be used to
+				// construct the url of each lecture's page
+				lectureId, _ := l.Attr("data-lecture-id")
 
-			// The video will be stored locally at the lectureFilePath
-			filePath := filepath.Join(sectionDir, lectureFileName)
+				lectureId = strings.TrimSpace(lectureId)
 
-			// We will need to visit the lecturePageUrl, to get the Wistia
-			// video download link
-			url := *courseUrlPtr + "/lectures/" + lectureId
+				lectureFileName := fmt.Sprint(getDashedName(lectureName, i), ".mp4")
 
-			newLectures = append(newLectures, Lecture{
-				name:     lectureName,
-				id:       lectureId,
-				filePath: filePath,
-				url:      url,
+				// The video will be stored locally at the lectureFilePath
+				filePath := filepath.Join(sectionDir, lectureFileName)
+
+				// We will need to visit the lecturePageUrl, to get the Wistia
+				// video download link
+				url := *courseUrlPtr + "/lectures/" + lectureId
+
+				newLectures = append(newLectures, Lecture{
+					name:     lectureName,
+					id:       lectureId,
+					filePath: filePath,
+					url:      url,
+				})
+
 			})
 
-		})
+			newSection := Section{
+				name:       name,
+				sectionDir: sectionDir,
+				lectures:   newLectures,
+			}
+			sections = append(sections, newSection)
 
-		newSection := Section{
-			name:       name,
-			sectionDir: sectionDir,
-			lectures:   newLectures,
 		}
-		sections = append(sections, newSection)
+		// This should provide rudamentary functionality to the optionalStartSectionNoPtr param
 
 	})
+
+	fmt.Println( "Done! Starting course download..." )
+	// Just providing the user with more feedback
 
 	// create course directory
 	os.Mkdir(courseDir, 0777)
@@ -231,6 +261,11 @@ func main() {
 
 			var wistiaVideoSize int64
 
+			var wistiaVideoSizeString string
+			var existingFileSizeOnDiskString string
+			// Create some new variables for the converted byte strings
+			//	It's all for readability, instead of placing the converted string straight into the printf
+
 			resp, err := client.Get(videoUrl)
 			if err != nil {
 				log.Fatal(err)
@@ -238,26 +273,34 @@ func main() {
 				contentLength := resp.Header.Get("Content-Length")
 				wistiaVideoSize, _ = strconv.ParseInt(contentLength, 10, 64)
 				// contentType := resp.Header.Get("Content-Type")
-				fmt.Printf("\n\t\twistiaVideoSize: %d", wistiaVideoSize)
+
+				wistiaVideoSizeString = bytefmt.ByteSize( uint64( wistiaVideoSize ) )
+				// Convert the video size to a human readable format and place it in a new var
+
+				fmt.Printf("\n\t\twistiaVideoSize: %s", wistiaVideoSizeString)
 
 			}
 
 			// check if video file already exists
 			if fileStat, err := os.Stat(lecture.filePath); err == nil {
 				existingFileSizeOnDisk := fileStat.Size()
-				fmt.Printf("\n\t\texistingFileSizeOnDisk: %d", existingFileSizeOnDisk)
+
+				existingFileSizeOnDiskString = bytefmt.ByteSize( uint64( existingFileSizeOnDisk ) )
+				// Convert the video size to a human readable format and place it in a new var
+
+				fmt.Printf("\n\t\texistingFileSizeOnDisk: %s", existingFileSizeOnDiskString)
 
 				if existingFileSizeOnDisk == wistiaVideoSize {
 					fmt.Println("\n\t\tFull video file exists; moving to next lecture...")
 					continue
 				} else {
-					fmt.Println("\n\t\tVideo file exists but not fully, will download again")
+					fmt.Printf("\n\t\tVideo file exists but not fully, will download again")
 				}
 			}
 
 			defer resp.Body.Close()
 
-			fmt.Println("\t\tDownloading video ...")
+			fmt.Printf("\n\t\tCreating file ...")
 
 			out, err := os.Create(lecture.filePath)
 			if err != nil {
@@ -265,11 +308,36 @@ func main() {
 			}
 			defer out.Close()
 
-			_, ioErr := io.Copy(out, resp.Body)
+			fmt.Printf(" Done!\n\t\tDownloading Video ... \n")
+
+			// Create the progress bar
+			bar := pb.New( int( wistiaVideoSize ) ).SetUnits( pb.U_BYTES ).SetRefreshRate( time.Second / 2 )
+			bar.ShowSpeed = true
+			bar.SetMaxWidth(80)
+			bar.Format("[=> ]")
+			// Custom init options
+
+			bar.Callback = func(progressBarString string) {
+				fmt.Printf("\r\t\t%v", progressBarString)
+			}
+			// Custom formatting callback, makes the progress bar stay on the same line, but have tabbing to keep inline with the other elements
+
+			bar.Start()
+			// Start the progress bar
+
+			writer := io.MultiWriter(out, bar)
+			// Create multi writer
+
+			_, ioErr := io.Copy(writer, resp.Body)
 			if ioErr != nil {
 				log.Fatal(err)
 			}
+			// Copy (download) the file
 
+			bar.Finish()
+			// Stop the progress bar
+
+			fmt.Printf("\t\tDone!")
 		}
 	}
 }
